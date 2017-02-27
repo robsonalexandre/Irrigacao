d0c1ad9f20eaa2e09704c0ec7b43f99acb3b1bad
Robson <alexandrerobson@gmail.com>
2017-02-26 21:34:26 -0300

    Adicionada funçoes para pegar Datetime e funções para uso do sendor DHT11

--- Irrigacao.ino
+++ Irrigacao.ino
@@ -1,40 +1,37 @@
 #include <Wire.h>
-#include "RTClib.h"
+#include <RTClib.h>
+#include <Timer.h>
+#include <SimpleDHT.h>
 
-#include "Timer.h"
-
+#define pinDHT11 A1
 #define Bomba 13
-#define Sensor 7
+#define Sensor A0
+#define SensorOnOff 7
+#define minUmidade 85
 
-int umidade = 30, ano, mes, dia, hora, minuto, segundo, bombaEvent;
-char data[20], horario[10], strUmidade[14],
+int bombaEvent;
+char data[20], horario[10],
   diasDaSemana[7][12] = {"Domingo", "Segunda", "Terca", "Quarta", "Quinta", "Sexta", "Sabado"},
-  agenda[5][12] = {"18:33:00", "18:35:00", "18:37:00", "18:39:00", "18:40:00"};
+  agenda[][12] = {
+    "21:33:00",
+    "21:35:00",
+    "21:37:00",
+    "21:29:00",
+    "21:30:00"
+  };
 
 RTC_DS3231 rtc;   //RTC_DS1307 rtc;
 Timer t;
+SimpleDHT11 dht11;
 
-void setup () {
-  pinMode(Bomba, OUTPUT);
-  pinMode(Sensor, OUTPUT);
-  digitalWrite(Bomba, LOW);                   // Desliga o solenóide
-  digitalWrite(Sensor, LOW);                  // Desliga o sensor
-  
-  Serial.begin(115200);
-  Serial.println("Comunicacao serial...");
-  delay(3000);
-
-  if (! rtc.begin()) {
-    Serial.println("RTC NAO esta rodando!");
-    while(1);
+void getDateTime(boolean saidaSerial = false){
+  DateTime now = rtc.now();
+  sprintf(data, "%s, %02d/%02d/%d", diasDaSemana[now.dayOfTheWeek()], now.day(), now.month(), now.year());
+  sprintf(horario, "%02d:%02d:%02d", now.hour(), now.minute(), now.second());
+  if (saidaSerial){
+    Serial.println(data);
+    Serial.println(horario);
   }
-  if (rtc.lostPower()) {
-    Serial.println("RTC sem bateria, ajustando data e hora!");
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-  }
-  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 }
 
 void desligaBomba() {
@@ -44,38 +41,58 @@ void desligaBomba() {
   t.stop(bombaEvent);
 }
 
-void loop () {  
-  DateTime now = rtc.now();
+void setup () {
+  pinMode(Bomba, OUTPUT);
+  pinMode(SensorOnOff, OUTPUT);
+  digitalWrite(Bomba, LOW);                   // Desliga o solenóide
+  digitalWrite(SensorOnOff, LOW);             // Desliga o sensor
+  
+  Serial.begin(115200);
+  delay(3000);
+
+  if (! rtc.begin()) {
+    Serial.println("RTC NAO esta rodando!");
+    while(1);
+  }
+  /*
+  if (rtc.lostPower()) {
+    Serial.println("RTC sem bateria, ajustando data e hora!");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+  /**/
+  getDateTime(true);
+}
+
+void loop () {
+  /* Para uso com o sensor de umidade de solo + Comparador LM393 */
+  int umidade;
+  /*  Para uso com o sensor DHT11 *\
+  byte umidade = 99
+  /**/
+  byte temperatura;
+  getDateTime(false);
   t.update();
   
-  dia = now.day();
-  mes = now.month();
-  ano = now.year();
-  hora = now.hour();
-  minuto = now.minute();
-  segundo = now.second();
-
-  sprintf(data, "%s, %02d/%02d/%d", diasDaSemana[now.dayOfTheWeek()], dia, mes, ano);
-  sprintf(horario, "%02d:%02d:%02d", hora, minuto, segundo);
-  /*
-  Serial.println(data);
-  Serial.println(horario);
-  */
-          //Verifica se está no horário do acionamento
+  //Verifica se está no horário do acionamento
   for (int i = 0; i < sizeof(agenda) - 1; i++){
     if (strcmp(horario, agenda[i]) == 0) {
-      digitalWrite(Sensor, HIGH); // Liga pino 7 para acionar o sensor
-      delay(2000);  // Aguarda 2s para esperar o sensor energizar e realizar a leitura
-      /*
-      umidade = analogRead(A0); 
+      digitalWrite(SensorOnOff, HIGH); // Liga pino 7 para acionar o sensor
+      delay(2000);  // Aguarda 2s esperando o sensor energizar e realizar a leitura
+      /* Para uso com o sensor de umidade de solo + Comparador LM393 */
+      umidade = analogRead(Sensor); 
       umidade = map(umidade, 0, 1023, 0, 100);
-      */
-      if (umidade < 35) {  //Se a umidade for menor que 35% liga o solenóide
+      /**/
+      /*  Para uso com o sensor DHT11 (var umidade deve ser byte) *\
+      dht11.read(pinDHT11, &temperatura, &umidade, NULL);
+      /**/       
+      if (umidade < minUmidade) {  //Se a umidade for menor que 35% liga o solenóide
         digitalWrite(Bomba, HIGH);  //Caso a umidade seja menor que 35% liga o pino=13 do solenoide
         int bombaEvent = t.after(35000, desligaBomba); // Após 35s desligará a solenoide
         Serial.println(horario);
-        sprintf(strUmidade, "Umidade: %d%%", umidade);
-        Serial.println(strUmidade);
+        Serial.print("Umidade: "); Serial.print(umidade); Serial.println("%");
+        Serial.print("Temperatura: "); Serial.print(temperatura); Serial.println("*C"); 
         Serial.println("Irrigacao INICIADA!");
       }
     }
